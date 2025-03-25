57f0f4a78e01b266a7995abec7a334443baddd7c
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-03-26 03:01:56 +0530

    learning-querySelectors

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 'use strict';
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
