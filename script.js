ff4ebd463ef1111788840d1ddc7ee63336fa3844
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-03-27 00:44:27 +0530

    learning-click-events

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 23;
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+  //storing  it in a variable
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number...:(';
+  } else {
+    document.querySelector('.message').textContent = '🎉 Correct answer!';
+  }
+});
