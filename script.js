5689535053fe24899301ad45ea294c25d4c7a924
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-03-27 22:32:34 +0530

    implemented-restart-feature

--- script.js
+++ script.js
@@ -39,3 +39,16 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
   }
 });
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.score').textContent = 20;
+});
